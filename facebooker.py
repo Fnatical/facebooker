dda78f36341f0d1da3b95e161f0c88be3ee8394c
Damon Claverie <damonclaverie@gmail.com>
2018-05-30 22:21:18 +1000

    Elasticsearch integration

--- facebooker.py
+++ facebooker.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3.6
 
-from twython import Twython  
+from twython import Twython
+from elasticsearch import Elasticsearch
+import requests
 import json
 import pprint
 
+es = Elasticsearch()
 pp = pprint.PrettyPrinter(indent=4)
 
 with open("twitter_credentials.json", "r") as file:  
     creds = json.load(file)
 
-python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
-#'geocode': '-28.0173,153.4257,200km',
-query = {'q': 'gold coast',  
-        'result_type': 'mixed',
-        'count': 99,
-        'lang': 'en',
+python_tweets = Twython(creds["CONSUMER_KEY"], creds["CONSUMER_SECRET"])
+#"geocode": "-28.0173,153.4257,200km",
+query = {"q": "gold coast tennis",  
+        "result_type": "mixed",
+        "count": 100,
+        "lang": "en",
         }
-    
+
 response = python_tweets.search(**query)
 
-for line in response['statuses']:
+i=0
+
+for line in response["statuses"]:
 
     keys = line.keys()
 
@@ -28,42 +33,49 @@ for line in response['statuses']:
     hashtags = []
     user_mentions = []
 
-    text = ''
+    text = ""
     followers_count = 0
-    name = ''
-    screen_name = ''
+    name = ""
+    screen_name = ""
 
-    if 'entities' in keys:
-        entity_keys = line['entities'].keys()
-        if 'hashtags' in entity_keys:
-            hashtags = line['entities']['hashtags']
-        if 'user_mentions' in entity_keys:
-            user_mentions = line['entities']['user_mentions']
+    if "entities" in keys:
+        entity_keys = line["entities"].keys()
+        if "hashtags" in entity_keys:
+            hashtags = line["entities"]["hashtags"]
+        if "user_mentions" in entity_keys:
+            user_mentions = line["entities"]["user_mentions"]
     
-    if 'text' in keys:
-        text = line['text']
+    if "text" in keys:
+        text = line["text"]
 
-    if 'user' in keys:
-        user = line['user']
+    if "user" in keys:
+        user = line["user"]
         user_keys = user.keys()
-        if 'followers_count' in user_keys:
-            followers_count = user['followers_count']
-        if 'name' in user_keys:
-            name = user['name']
-        if 'screen_name' in user_keys:
-            screen_name = user['screen_name']
-            
+        if "followers_count" in user_keys:
+            followers_count = user["followers_count"]
+        if "name" in user_keys:
+            name = user["name"]
+        if "screen_name" in user_keys:
+            screen_name = user["screen_name"]
+
     if len(hashtags) > 0:
-        obj['hashtags'] = []
+        obj["hashtags"] = []
         for hashtag in hashtags:
-            obj['hashtags'].append(hashtag['text'])
+            obj["hashtags"].append(hashtag["text"])
     if len(user_mentions) > 0:
-        obj['user_mentions'] = []
+        obj["user_mentions"] = []
         for user_mention in user_mentions:
-            obj['user_mentions'].append({'name': user_mention['name'], 'screen_name': user_mention['screen_name']})
+            obj["user_mentions"].append({"name": user_mention["name"], "screen_name": user_mention["screen_name"]})
 
 
-    obj['name'] = name
-    obj['screen_name'] = screen_name
-    obj['followers'] = followers_count
-    obj['text'] = text
+    obj["name"] = name
+    obj["screen_name"] = screen_name
+    obj["followers"] = followers_count
+    obj["text"] = text
+
+    #es.index(index="twitter_tennis", doc_type="gold_coast_tennis", id=i, body=obj)
+
+    #i+=1
+
+a = es.search(index='twitter_tennis', doc_type='gold_coast_tennis')
+print(a)
